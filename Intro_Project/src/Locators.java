7d0fc2c4b63ea601372abff570c5a7a43b440b9d
Gaurav Sinha <grvsinha998@gmail.com>
2022-04-17 23:38:03 +0530

    17-04

--- Intro_Project/src/Locators.java
+++ Intro_Project/src/Locators.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		 System.setProperty("webdriver.chrome.driver", "/Users/gauravsinha/Downloads/chromedriver");
@@ -16,16 +16,31 @@ public class Locators {
 		 
 		 driver.findElement(By.id("inputUsername")).sendKeys("user@phptravels.com");
 		 driver.findElement(By.name("inputPassword")).sendKeys("demouser");
-		 driver.findElement(By.className("submit")).click();
+		 driver.findElement(By.className("submit")).click();		 
+		 System.out.println(driver.findElement(By.cssSelector("p.error")).getText());		
 		 
-		 System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-		
 		 driver.findElement(By.linkText("Forgot your password?")).click();
+		 Thread.sleep(2000);
+		 
 		 driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Gaurav_Sinha");
 		 driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("mustang.gaurav@gmail.com");
 		 driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("0123456789");
-		 driver.findElement(By.xpath("//form//div//button[text()='Reset Login']")).click();
+		 driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		 System.out.println(driver.findElement(By.cssSelector(".infoMsg")).getText());
 		 
+		 driver.findElement(By.cssSelector(".go-to-login-btn")).click();
+		 Thread.sleep(2000);
+		 
+		 driver.findElement(By.id("inputUsername")).sendKeys("Gaurav");
+		 driver.findElement(By.cssSelector("input[type*=pass]")).sendKeys("rahulshettyacademy");
+		 driver.findElement(By.className("submit")).click();
+		 
+		 Thread.sleep(2000);
+		 System.out.println(driver.findElement(By.xpath("//div[@class='login-container']/p")).getText());
+		 
+		 driver.findElement(By.cssSelector(".logout-btn")).click();
+		 
+		 Thread.sleep(2000);
 		 driver.quit();
 		 
 		 }
