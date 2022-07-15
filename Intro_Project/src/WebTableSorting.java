1662dbf065b3b26e94fc084faeb7a95b7c4e73ee
Gaurav Sinha <grvsinha998@gmail.com>
2022-07-16 01:10:11 +0530

    23/06

--- Intro_Project/src/WebTableSorting.java
+++ Intro_Project/src/WebTableSorting.java
@@ -18,55 +18,26 @@ public class WebTableSorting {
 
         driver.findElement(By.xpath("//tr/th[1]")).click();
 
-
-
-// capture all webelements into list
-
         List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
-
-
-
-// capture text of all webelements into new(original) list
-
         List<String> originalList = elementsList.stream().map(WebElement::getText).collect(Collectors.toList());
-
-
-
-// sort on the original list of step 3 -> sorted list
-
-
-
         List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
-
-// compare original list vs sorted list
-
         Assert.assertTrue(new HashSet<>(originalList).containsAll(sortedList));
 
         List<String> price;
 
-// scan the name column with getText ->Beans->print the price of the Rice
-
         do
-
         {
-
             List<WebElement> rows = driver.findElements(By.xpath("//tr/td[1]"));
-
             price = rows.stream().filter(s -> s.getText().contains("Rice"))
-
                     .map(WebTableSorting::getPriceVeggie).collect(Collectors.toList());
-
             price.forEach(System.out::println);
 
             if(price.size()<1)
-
             {
-
                 driver.findElement(By.cssSelector("[aria-label='Next']")).click();
-
             }
-
-        }while(price.size()<1);
+        }
+        while(price.size()<1);
 
         driver.quit();
 
